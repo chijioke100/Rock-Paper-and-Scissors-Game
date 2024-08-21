5278f6bb39ce8468c782e1a7e0c11c3e3ae76ce3
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2024-08-21 20:58:51 +0000

    add random number function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,14 @@
 
 const selections = ['rock', 'paper', 'scissors'];
 const player = document.getElementById("player");
-const computer = document.getElementById("computer")
-const outcome = document.getElementById("outcome")
+const computer = document.getElementById("computer");
+const outcome = document.getElementById("outcome");
+
+// function that accept one argument
+function gameOn(selectOn) {
+
+// Generate a Random Number for Rock, Paper and Scissors
+    const computerSelect = selections[Math.floor(Math.random() * 3)];
+
+
+}
