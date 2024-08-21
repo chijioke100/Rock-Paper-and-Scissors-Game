3c18c7e906d010f6aba0c9dfae9a1ece16dde545
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2024-08-21 20:48:44 +0000

    add declaration of const

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Declare
+
+const selections = ['rock', 'paper', 'scissors'];
+const player = document.getElementById("player");
+const computer = document.getElementById("computer")
+const outcome = document.getElementById("outcome")
