9b31f88cab75cead0cf80e67d040fcf04cfd123c
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2024-08-21 21:17:59 +0000

    add switch statement for options

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,24 @@ function gameOn(selectOn) {
 
 // Generate a Random Number for Rock, Paper and Scissors
     const computerSelect = selections[Math.floor(Math.random() * 3)];
+    let outcome = "";
+
+    if (gameOn === computerSelect){
+        outcome = "Draw";
+    }
+    else {
+        switch (gameOn) {
+            case "rock":
+            outcome = (computerSelect === "scissors") ? "WIN!" : "LOSE!";
+            break;
+            case "paper":
+            outcome = (computerSelect === "rock") ? "WIN!" : "LOSE!";
+            break;
+            case "scissors":
+            outcome = (computerSelect === "paper") ? "WIN!" : "LOSE!";
+            break;
+        }
+    }
 
 
 }
